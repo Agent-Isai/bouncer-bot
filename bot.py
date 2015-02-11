5330b6a818c7f0f1ec77b4fcd7cb9f91cae46f6f
Polsaker <bouramiro@gmail.com>
2015-02-11 19:51:52 -0300

    Added almost all the admin commands

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ import time
 import json
 import logging
 import smtplib
+import random
+import string
+import re
 from email.mime.text import MIMEText
 
 
@@ -14,6 +17,8 @@ logging.basicConfig()
 
 # load the config...
 config = json.load(open("config.json"))
+
+_EMAIL_REGEX = re.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 ircs = {} # IRC connections
 baks = {} # Backend connections
@@ -47,6 +52,9 @@ class NetworkThing(object):
                 # TODO: Check the email against blacklists, etc
                 # TODO: Check the network against blacklists too
                 # TODO: Check for banned/rejected users
+                if not _EMAIL_REGEX.match(ev.splitd[3]):
+                    cli.privmsg(ev.target, "Invalid email address")
+                    return
                 
                 if ":" in ev.splitd[2]:
                     cli.privmsg(ev.target, "Please do not introduce the port in the network address")
@@ -70,11 +78,12 @@ class NetworkThing(object):
                     self.nets['_ADMIN_NETWORK_'].privmsg(self.config['servers'][self.nets['_ADMIN_NETWORK_'].sid]['admin-channel'], "INFO: Unknown network. Add it with \002!aliasnet <address> <name>\002, and if the network does not exist: \002!addnet <name> <address> <port (ssl if possible)>")
             
             # TODO: !report command to report bouncers
+            # TODO: !reqnet to request another network
             # TODO: !resetpass command to reset the user's password
 
         elif ev.target.lower() == self.myconf['admin-channel']:
             if ev.splitd[0] == "!aliasnet":
-                if len(ev.splitd[0]) < 2:
+                if len(ev.splitd) < 3:
                     cli.privmsg(ev.target, "Usage: !aliasnet <address> <network>")
                     return
                 
@@ -88,14 +97,14 @@ class NetworkThing(object):
                 net.save()
                 cli.privmsg(ev.target, "Address saved.")
             elif ev.splitd[0] == "!addnet":
-                if len(ev.splitd[0]) < 3:
+                if len(ev.splitd) < 4:
                     cli.privmsg(ev.target, "Usage: !addnet <name> <address> <port>")
                     return
                 net = Network.create(name=ev.splitd[1], address=ev.splitd[2], port=ev.splitd[3])
                 net.save()
                 cli.privmsg(ev.target, "Network saved.")
             elif ev.splitd[0] == "!reject":
-                if len(ev.splitd[0]) < 2:
+                if len(ev.splitd) < 3:
                     cli.privmsg(ev.target, "Usage: !reject <id> <reason>")
                     return
                 try:
@@ -113,12 +122,50 @@ class NetworkThing(object):
                 s = smtplib.SMTP('localhost')
                 s.send_message(msg)
                 s.quit()
+                self.nets[req.on].privmsg(self.config['servers'][req.on]['request-channel'], req.nick + "'s request was rejected. Please check your email for more information")
                 req.delete_instance()
-                cli.privmsg(ev.source, "Request rejected.")
+                cli.privmsg(ev.target, "Request rejected.")
+            elif ev.splitd[0] == "!accept":
+                if len(ev.splitd) < 2:
+                    cli.privmsg(ev.target, "Usage: !accept <id> [server]")
+                    return
+                try:
+                    req = PendingRequest.get(PendingRequest.id == int(ev.splitd[1]))
+                except:
+                    cli.privmsg(ev.target, "Request not found")
+                    return
+                
+                try:
+                    netw = NetworkAddr.get(NetworkAddr.address == req.network)
+                except:
+                    cli.privmsg(ev.target, "Unknown network.")
+                    return
+                
+                netw = Network.get(Network.name==netw.name)
+                
+                if len(ev.splitd) < 3:
+                    server = ev.splitd[2]
+                else:
+                    server = random.choice(list(baks))
+                
+                password = ''.join(random.choice(string.ascii_lowercase) for i in range(12))
+                baks[server].adduser(req.user, password)
+                baks[server].addnetwork(req.user, netw.name, netw.address + " " + netw.port)
+                text = "Dear {0},\n\nYour bouncer request for {1} was approved!\nCredentials:\n - Server: {2}\n - Port: 1337 (+1338 for ssl)\n - User: {3}\n - Password: {4}\n\n -- The Hira bouncer service staff".format(req.user, req.network, server + ".bouncers.pw", req.user, password)
+                msg = MIMEText(text)
+                msg['Subject'] = "Hira bouncer service"
+                msg['From'] = "noreply@bouncers.pw"
+                msg['To'] = req.email
+                s = smtplib.SMTP('localhost')
+                s.send_message(msg)
+                s.quit()
+                User.create(email = req.email, nick = req.user)
+                UserNetworks.create(nick = req.user, network = netw.name)
+                self.nets[req.on].privmsg(self.config['servers'][req.on]['request-channel'], req.nick + "'s request was approved. Please check your email for more information")
+                req.delete_instance()
+                cli.privmsg(ev.target, "Request approved")
 
-            # TODO: !accept command to accept bouncers
-            # TODO: !reject command to reject bouncers
-
+            # TODO: !deluser to delete a user
 class BackendThing(object):
     def __init__(self, irc, sid):
         self.irc = irc
@@ -133,7 +180,7 @@ class BackendThing(object):
     def addnetwork(self, username, network, server):
         self.irc.privmsg("*controlpanel", "addnetwork {0} {1}".format(username, network))
         self.irc.privmsg("*controlpanel", "addserver {0} {1} {2}".format(username, network, server))
-        self.irc.privmsg("*controlpanel", "addchan {0} {1} #HiraBNC".format(username, network, server))
+        self.irc.privmsg("*controlpanel", "addchan {0} {1} #AwesomeBNC".format(username, network, server))
     
 database = peewee.SqliteDatabase('bouncers.db')
 database.connect()
@@ -145,6 +192,11 @@ class BaseModel(peewee.Model):
 class User(BaseModel): # ZNC user object or something like that
     nick = peewee.CharField() # username
     email = peewee.CharField() # email
+
+class UserNetworks(BaseModel): # User<->network object
+    nick = peewee.CharField() # username
+    network = peewee.CharField() # network name
+
     
 class PendingRequest(BaseModel): # Pending request model
     nick = peewee.CharField() # User's nick
@@ -165,6 +217,7 @@ class NetworkAddr(BaseModel): # known addresses from networks
 
 PendingRequest.create_table(True)
 User.create_table(True)
+UserNetworks.create_table(True)
 Network.create_table(True)
 NetworkAddr.create_table(True)
 
@@ -187,6 +240,8 @@ for i in config['backends']:
                     password = config['backends'][i]['password'])
         temp.connect()
         baks[i] = BackendThing(temp, i)
+    elif config['backends'][i]['type'] == "local":
+        baks[i] = BackendThing(ircs['_ADMIN_NETWORK_'], i)
         
 
 while True:
